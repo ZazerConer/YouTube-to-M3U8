1f0aaba1213f58001295050faee564c54beca1e7
Zazer Coner's <96803085+ZazerConer@users.noreply.github.com>
2022-06-21 03:04:04 +0800

    Create youtube_m3ugrabber.py

---
+++ scripts/youtube_m3ugrabber.py
@@ -0,0 +1,76 @@
+#! /usr/bin/python3
+
+banner = r'''
+###########################################################################
+#      ____ 	____    _____                                             #
+#     |  _ \   |       |  _   \                                           #
+#     | |_) |  |____   | |_|  |                                           #
+#     |  __/        |  |  _   /                                           #
+#     |_|       ____|  |_| |__\                                           #
+#                                                                         #
+#                                  >> https://github.com/ZazerConer       #
+###########################################################################
+'''
+
+
+#EXTINF:-1 group-title="Info - Must Read" tvg-logo="https://i.imgur.com/eoRVb0I.jpg" tvg-id="", Playlist is for Free
+https://raw.githubusercontent.com/ZazerConer/UTLive/main/assets/info.m3u8
+#EXTINF:-1 group-title="Malay" tvg-logo="https://i.imgur.com/fMZRciY.jpg" tvg-id="", Suke TV
+
+'''
+
+import requests
+import os
+import sys
+
+windows = False
+if 'win' in sys.platform:
+    windows = True
+
+def grab(url):
+    response = s.get(url, timeout=15).text
+    if '.m3u8' not in response:
+        response = requests.get(url).text
+        if '.m3u8' not in response:
+            if windows:
+                print('https://raw.githubusercontent.com/ZazerConer/YouTube_to_m3u/main/assets/info.m3u8')
+                return
+            #os.system(f'wget {url} -O temp.txt')
+            os.system(f'curl "{url}" > temp.txt')
+            response = ''.join(open('temp.txt').readlines())
+            if '.m3u8' not in response:
+                print('https://raw.githubusercontent.com/ZazerConer/YouTube_to_m3u/main/assets/info.m3u8')
+                return
+    end = response.find('.m3u8') + 5
+    tuner = 100
+    while True:
+        if 'https://' in response[end-tuner : end]:
+            link = response[end-tuner : end]
+            start = link.find('https://')
+            end = link.find('.m3u8') + 5
+            break
+        else:
+            tuner += 5
+    print(f"{link[start : end]}")
+
+print('#EXTM3U x-tvg-url="https://weareblahs.github.io/epg/mytv.xml"')
+print(banner)
+s = requests.Session()
+with open('../youtube_channel_info.txt') as f:
+    for line in f:
+        line = line.strip()
+        if not line or line.startswith('~~'):
+            continue
+        if not line.startswith('https:'):
+            line = line.split('|')
+            ch_name = line[0].strip()
+            grp_title = line[1].strip().title()
+            tvg_logo = line[2].strip()
+            tvg_id = line[3].strip()
+            print(f'\n#EXTINF:-1 group-title="{grp_title}" tvg-logo="{tvg_logo}" tvg-id="{tvg_id}", {ch_name}')
+        else:
+            grab(line)
+            
+if 'temp.txt' in os.listdir():
+    os.system('rm temp.txt')
+    os.system('rm watch*')
