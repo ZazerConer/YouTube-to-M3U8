78972ca3a608a2e28a4b4e48a964e63737f06c84
Zazer Coner's <96803085+ZazerConer@users.noreply.github.com>
2022-06-21 06:33:53 +0800

    Update m3u.py

--- scripts/m3u.py
+++ scripts/m3u.py
@@ -15,12 +15,12 @@ def grab(url):
         #response = requests.get(url).text
         if '.m3u8' not in response:
             if windows:
-                print('https://raw.githubusercontent.com/ZazerConer/assets/.m3u8')
+                print('https://raw.githubusercontent.com/ZazerConer/assets/info.m3u8')
                 return
             os.system(f'wget {url} -O temp.txt')
             response = ''.join(open('temp.txt').readlines())
             if '.m3u8' not in response:
-                print('https://raw.githubusercontent.com/ZazerConer/assets/.m3u8')
+                print('https://raw.githubusercontent.com/ZazerConer/assets/info.m3u8')
                 return
     end = response.find('.m3u8') + 5
     tuner = 100
