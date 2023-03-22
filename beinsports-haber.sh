a2f8e07328917498bde354dfd1903190299cd49e
ZazerConer's <96803085+ZazerConer@users.noreply.github.com>
2023-03-23 05:34:00 +0800

    Update and rename SukeTV.sh to beinsports-haber.sh

--- beinsports-haber.sh
+++ beinsports-haber.sh
@@ -6,6 +6,6 @@ python3 -m pip install requests
 
 cd $(dirname $0)/scripts/
 
-python3 SukeTV.py > ../SukeTV.m3u8
+python3 beinsports-haber.py > ../beinsports-haber.m3u8
 
-echo m3u grabbed
+echo m3u8 grabbed
