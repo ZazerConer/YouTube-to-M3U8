8b21d07ae3d001bb1950a504bb950815143962a0
ZazerConer's <96803085+ZazerConer@users.noreply.github.com>
2024-07-04 13:26:21 +0800

    Update handle-name.js

--- youtube-live-url/workers/handle-name.js
+++ youtube-live-url/workers/handle-name.js
@@ -1,39 +1,39 @@
 addEventListener('fetch', (event) => {
   event.respondWith(
     handleRequest(event.request).catch(
-      (err) => new Response(err.message, { status: 500 })
+      (err) => new Response(err.message, {status: 500})
     )
   )
-})
+});
 
-async function handleRequest (request) {
-  const { pathname } = new URL(request.url)
+async function handleRequest(request) {
+  const {pathname} = new URL(request.url);
 
   if (pathname.startsWith('/@')) {
-      const handle = pathname.split('.')?.[0]
+      const handleName = pathname.split('.')[0];
     
-    if (handle !== '') { 
-      const url = `https://www.youtube.com/${handle}/live`
+    if (handleName !== '') { 
+      const url = `https://www.youtube.com/${handleName}/live`;
 
       const response = await fetch(url, {
         cf: {
           cacheTtl: 10800,
           cacheEverything: true
         }
-      })
+      });
 
       if (response.ok) {
-        const text = await response.text()
-        const stream = text.match(/(?<=hlsManifestUrl":").*\.m3u8/g)
+        const text = await response.text();
+        const stream = text.match(/(?<=hlsManifestUrl":").*\.m3u8/g);
 
-        return Response.redirect(stream, 302)
+        return Response.redirect(stream, 302);
       } else {
-        throw Error(`Youtube URL (${url}) failed with status: ${response.status}`)
+        throw Error(`Youtube URL (${url}) failed with status: ${response.status}`);
       }
     } else {
-      throw Error(`Channel Name not found: ${pathname}`)
+      throw Error(`Channel Name not found: ${pathname}`);
     }
   } else {
-    throw Error(`Path not found: ${pathname}`)
+    throw Error(`Path not found: ${pathname}`);
   }
 }
