1553c88c65db20f3b2727dc7be31615f6bb1382d
ZazerConer's <96803085+ZazerConer@users.noreply.github.com>
2024-07-04 13:24:14 +0800

    Update video-id.js

--- youtube-live-url/workers/video-id.js
+++ youtube-live-url/workers/video-id.js
@@ -1,39 +1,39 @@
 addEventListener('fetch', (event) => {
   event.respondWith(
     handleRequest(event.request).catch(
-      (err) => new Response(err.message, { status: 500 })
+      (err) => new Response(err.message, {status: 500})
     )
   )
-})
+});
 
-async function handleRequest (request) {
-  const { pathname } = new URL(request.url)
+async function handleRequest(request) {
+  const {pathname} = new URL(request.url);
 
   if (pathname.startsWith('/live/')) {
-      const videoId = pathname.split('/')?.[2]?.split('.')?.[0]
+      const videoID = pathname.split('/')[2].split('.')[0];
     
-    if (videoId !== '') { 
-      const url = `https://www.youtube.com/live/${videoId}/live`
+    if (videoID !== '') { 
+      const url = `https://www.youtube.com/live/${videoID}/live`;
 
       const response = await fetch(url, {
         cf: {
           cacheTtl: 10800,
           cacheEverything: true
         }
-      })
+      });
 
       if (response.ok) {
-        const text = await response.text()
-        const stream = text.match(/(?<=hlsManifestUrl":").*\.m3u8/g)
+        const text = await response.text();
+        const stream = text.match(/(?<=hlsManifestUrl":").*\.m3u8/g);
 
-        return Response.redirect(stream, 302)
+        return Response.redirect(stream, 302);
       } else {
-        throw Error(`Youtube URL (${url}) failed with status: ${response.status}`)
+        throw Error(`Youtube URL (${url}) failed with status: ${response.status}`);
       }
     } else {
-      throw Error(`Video ID not found: ${pathname}`)
+      throw Error(`Video ID not found: ${pathname}`);
     }
   } else {
-    throw Error(`Path not found: ${pathname}`)
+    throw Error(`Path not found: ${pathname}`);
   }
 }
