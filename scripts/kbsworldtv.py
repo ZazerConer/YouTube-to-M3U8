a4629e9add7a0136ccfbcbaca37bb0b7a70f828d
ZazerConer's <96803085+ZazerConer@users.noreply.github.com>
2024-09-18 21:22:25 +0800

    Update and rename beinsports-haber.py to kbsworldtv.py

--- scripts/kbsworldtv.py
+++ scripts/kbsworldtv.py
@@ -1,6 +1,3 @@
-#! /usr/bin/python3
-
-
 import requests
 import os
 import sys
@@ -43,7 +40,7 @@ print('#EXTM3U')
 print('#EXT-X-VERSION:3')
 print('#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=2560000')
 s = requests.Session()
-with open('../beinsports-haber.txt') as f:
+with open('../kbsworldtv.txt') as f:
     for line in f:
         line = line.strip()
         if not line or line.startswith('~~'):
